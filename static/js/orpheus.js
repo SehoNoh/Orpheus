f9b7b527e729e03f9242562c59fec497f72d10ea
NohSeho <seho.noh@sk.com>
2015-03-19 19:09:07 +0900

    Google Analytics add

--- static/js/orpheus.js
+++ static/js/orpheus.js
@@ -55,39 +55,55 @@ $("#search").keypress(function(event) {
     if (event.which == 13) {
         // alert("");
         event.preventDefault();
-        $("#header").addClass('loading');
+
+        if ($(this).val() === '') {
+                    ga('send', 'event', 'query', 'blank search');
+      alert('Insert Search Query');
+        } else {
+   $("#header").addClass('loading');
 
         var dom = '';
         var START_TAG = '<div class="music_card" id="';
-        var CENTER_TAG = '""><img src="';
+        var CENTER_TAG = '" title="';
+        var CENTER_2_TAG ='"><img src="';
         var END_TAG = '"/></div>'
+        var query = $(this).val();
 
-        $.get('/load?query=' + $(this).val(), function(data) {
+        $.get('/load?query=' + query, function(data) {
+            ga('send', 'event', 'query', 'search', query);
             list = JSON.parse(JSON.stringify(data));
 
             $.each(list, function(key, value) {
                 dom += START_TAG;
                 dom += key;
-                dom += CENTER_TAG
+                dom += CENTER_TAG;
+                dom += value.title;
+                dom += CENTER_2_TAG;
                 dom += value.imgSrc;
                 dom += END_TAG;
 
                 console.log(key + ': ' + value.imgSrc + ', ' + value.title);
             });
 
+            console.log("DOM : " + dom);
+
             $("#container").html(dom);
 
             $(".music_card").click(function() {
                 var uniqueId = $(this).attr("id");
+                var title = $(this).attr("title");
                 $.get("/video/" + uniqueId, function(data) {
+                    ga('send', 'event', 'player', 'play', ('TITLE : "' + title + '" / Unique ID : "' + uniqueId + '"'));
                     src = JSON.parse(data);
                     console.log(src["src"]);
                     $("#player").attr("src", src["src"]);
                     $("#player").attr("uniqueId", uniqueId);
+                    $("#player").attr("title", title);
                     playlist.push(uniqueId);
                     alert(playlist);
                 });
             });
-        });
+        });           
+        }
     }
 });
